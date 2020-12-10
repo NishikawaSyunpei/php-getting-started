740dddf09899f6c5757579b44cc7368f6e999298
NishikawaSyunpei <nishikawa_syunpei@adore.bz>
2020-12-10 16:18:47 +0900

    Update asagohan.php

--- web/views/asagohan.php
+++ web/views/asagohan.php
@@ -12,3 +12,16 @@ if($_POST['seibetsu']=='otoko'){
 }
 $answer=$rimit*365;
 ?>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>おいしく朝ごはんを食べれる日数</title>
+</head>
+<h1>おいしく朝ごはんを食べれる日数</h1><br><br>
+<body>朝ごはんをおいしく食べられる日数は<br>
+<?php
+	echo"$answer"
+	?>
+です！
+</body>
+</html>
