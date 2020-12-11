cb4a1ebf0f3f7bf53e5af06cd3d6629353cdf022
NishikawaSyunpei <nishikawa_syunpei@adore.bz>
2020-12-11 15:41:01 +0900

    Update asagohan.php

--- web/views/asagohan.php
+++ web/views/asagohan.php
@@ -1,27 +1,39 @@
-<?php
-$Nenrei=int($_POST['toshi']);
-$DanAveJumyo=81;
-$JoAveJumyo=87;
-if($_POST['seibetsu']=='otoko'){
-	$seibetsu="男";
-	$rimit=81-$Nenrei;
-	
-}else{
-	$seibestu="女"
-	$rimit=$Nenrei-87;
-}
-$answer=$rimit*365;
-?>
 <html lang="ja">
 <head>
 <meta charset="utf-8">
 <title>おいしく朝ごはんを食べれる日数</title>
 </head>
 <h1>おいしく朝ごはんを食べれる日数</h1><br><br>
-<body>朝ごはんをおいしく食べられる日数は<br>
+<body>
+<?php if (isset($_GET['button'])): ?>
 <?php
-	echo"$answer"
-	?>
-です！
+$Nenrei=int($_GET['toshi']);
+$DanAveJumyo=81;
+$JoAveJumyo=87;
+<?php if ($_GET['seibetsu']=='otoko') {
+	$seibetsu="男";
+	$rimit=81-$Nenrei;
+	
+}else{
+	$seibestu="女"
+	$rimit=$Nenrei-87;
+
+$answer=$rimit*365;
+
+?>
+
+朝ごはんをおいしく食べられる日数は<br>
+<?php echo $answer+"日です！"; ?>
+<?php else: ?>
+<form action="https://herokuasagohan.herokuapp.com" method="get">
+	<p>年齢を入力してください：<br>
+	<input type="text" name="toshi" id="toshi"></p><br>
+	<p>性別：<br>
+	<label><input type="radio" name="seibetsu" value="otoko" id="seibetsu" >男性</label>
+	<label><input type="radio" name="seibetsu" value="onna" id="seibetsu" >女性</label></p>
+	<p><input type="submit" name="button" value="決定"><input type="reset" value="クリア"></p>
+	
+</form>
+<?php endif; ?>
 </body>
 </html>
