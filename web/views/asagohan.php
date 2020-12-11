cca576d7007a9e5d127b97ec4e177facf7007836
NishikawaSyunpei <nishikawa_syunpei@adore.bz>
2020-12-11 16:21:21 +0900

    Update asagohan.php

--- web/views/asagohan.php
+++ web/views/asagohan.php
@@ -25,7 +25,7 @@ echo "朝ごはんをおいしく食べられる日数は\n"
 echo $answer+"日です！"; 
 ?>
 <?php else: ?>
-<form action="https://herokuasagohan.herokuapp.com/asagohan.php" method="get">
+<form action="php-getting-started/web/views/asagohan.php " method="get">
 	<p>年齢を入力してください：<br>
 	<input type="text" name="toshi" id="toshi"></p><br>
 	<p>性別：<br>
