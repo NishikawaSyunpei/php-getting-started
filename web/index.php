01a11824590fac06377ea21a238fefceb57518bd
NishikawaSyunpei <nishikawa_syunpei@adore.bz>
2020-12-14 14:29:56 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,17 +7,17 @@
 <h1>おいしく朝ごはんを食べれる日数</h1><br><br>
 <body>
 <?php 
-if (isset($_GET['button'])):{
-	$Nenrei=$_GET['toshi'];
+if (isset($_POST['button'])):{
+	$Nenrei=$_POST['toshi'];
 	$DanAveJumyo=81;
 	$JoAveJumyo=87;
-	if ($_GET['seibetsu']=='otoko'&& $Nenrei<81) {
+	if ($_POST['seibetsu']=='otoko'&& $Nenrei<81) {
 		$rimit=81-$Nenrei;
 		$answer=$rimit*365;
 		echo "朝ごはんをおいしく食べられる日数は\n";
 		echo $answer,"日です！";
 		
-	}else if($_GET['seibetsu']=='onna'&& $Nenrei<87){
+	}else if($_POST['seibetsu']=='onna'&& $Nenrei<87){
 		$rimit=87-$Nenrei;
 		$answer=$rimit*365;
 		echo "朝ごはんをおいしく食べられる日数は\n";
@@ -28,7 +28,7 @@ if (isset($_GET['button'])):{
 }
 ?>
 <?php else: ?>
-<form action="#" method="get">
+<form action="#" method="post">
 	<p>年齢を入力してください：<br>
 	<input type="text" name="toshi" id="toshi"></p><br>
 	<p>性別：<br>
