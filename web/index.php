362051e0846bba49da0f877924bccf8db2f4cc36
NishikawaSyunpei <nishikawa_syunpei@adore.bz>
2020-12-14 14:06:40 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,25 +1,38 @@
-<?php
-
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>おいしく朝ごはんを食べれる日数</title>
+</head>
+<h1>おいしく朝ごはんを食べれる日数</h1><br><br>
+<body>
+<?php 
+if (isset($_GET['button'])):{
+	$Nenrei=$_GET['toshi'];
+	$DanAveJumyo=81;
+	$JoAveJumyo=87;
+	if ($_GET['seibetsu']=='otoko') {
+		$rimit=81-$Nenrei;
+	
+	}else{
+		$rimit=$Nenrei-87;
+	}
+	$answer=$rimit*365;
+	
+	echo "朝ごはんをおいしく食べられる日数は\n";
+	echo $answer+"日です！"; 
+}
+?>
+<?php else: ?>
+<form action="#" method="get">
+	<p>年齢を入力してください：<br>
+	<input type="text" name="toshi" id="toshi"></p><br>
+	<p>性別：<br>
+	<label><input type="radio" name="seibetsu" value="otoko" id="seibetsu" >男性</label>
+	<label><input type="radio" name="seibetsu" value="onna" id="seibetsu" >女性</label></p>
+	<p><input type="submit" name="button" value="決定"><input type="reset" value="クリア"></p>
+	
+</form>
+<?php endif ;?>
+</body>
+</html>
