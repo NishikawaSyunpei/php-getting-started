a3f2564db93afcc5292b4d7598a67128367e7394
NishikawaSyunpei <nishikawa_syunpei@adore.bz>
2021-03-09 17:37:50 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -35,7 +35,7 @@ if (isset($_POST['button'])):{
 	<label><input type="radio" name="seibetsu" value="otoko" id="seibetsu" >男性</label>
 	<label><input type="radio" name="seibetsu" value="onna" id="seibetsu" >女性</label></p>
 	<p><input type="checkbox" name="urudoshi" value="urudoshi">うるう年を計算する</p>
-	<p><input type="submit" name="button" value="決定"><input type="reset" value="クリア"></p>
+	<p><input type="submit" name="button" value="決定">	<input type="reset" value="クリア"></p>
 	
 </form>
 <?php endif ;?>
